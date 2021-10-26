f308b8a1a86e196e2cbf3211edfcb46623253402
Jevaun Harris <jh601125@digicelgroup.com>
2021-10-25 23:42:51 -0500

    Initial commit

---
+++ src/main/java/com/harris/service/musicplayer/MusicPlayerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.harris.service.musicplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlayerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlayerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harris/service/musicplayer/MusicPlayerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harris.service.musicplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
